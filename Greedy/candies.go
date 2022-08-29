fca4481873b3f09ac4558608287023f9705bbab0
ajay <ajay.m.arkeri07@gmail.com>
2022-08-29 17:34:06 +0530

    Candies Min and Max

---
+++ Greedy/candies.go
@@ -0,0 +1,34 @@
+package greedy
+
+import "sort"
+
+// candies problem from GFG --> Find the minimum and maximum amount to buy all N candies
+
+func Amount(arr []int, N int, k int) (int, int) {
+	sort.Ints(arr)
+	max := 0
+	min := 0
+
+	//minimum
+	buy := 0
+	free := N - 1
+
+	for buy <= free {
+		min = min + arr[buy]
+		buy++
+		free -= k
+	}
+
+	//Maximum
+	buy = N - 1
+	free = 0
+
+	for buy >= free {
+		max = max + arr[buy]
+		buy--
+		free += k
+	}
+
+	return min, max
+
+}
